d0647e479731a3dde5e92055b3c49de6f1bc5742
Juarez Neto <juaresnetof@gmail.com>
2025-02-24 19:15:34 -0300

    Update views.py

--- estoque_livros/livros/views.py
+++ estoque_livros/livros/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
-from .models import Livro, Autor, Estoque
-from .forms import LivroForm, AutorForm, EstoqueForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Livro, Autor
+from .forms import LivroForm, AutorForm
 
+# Página inicial
 def index(request):
     return render(request, 'index.html')
 
@@ -11,7 +12,7 @@ def cadastrar_autor(request):
         form = AutorForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('cadastrar_autor')
+            return redirect('lista_cadastros')  # Redireciona para a lista de cadastros após salvar
     else:
         form = AutorForm()
     return render(request, 'cadastro_autor.html', {'form': form})
@@ -22,12 +23,39 @@ def cadastrar_livro(request):
         form = LivroForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('lista_cadastros')
+            return redirect('lista_cadastros')  # Redireciona para a lista de livros após salvar
     else:
         form = LivroForm()
     return render(request, 'cadastro_livro.html', {'form': form})
 
 # Lista de Cadastros
 def lista_cadastros(request):
-    livros = Livro.objects.select_related('autor').all()
+    livros = Livro.objects.select_related('autor').all()  # Busca os livros com seus autores
     return render(request, 'lista_cadastros.html', {'livros': livros})
+
+# Excluir Autor (exclui o autor e todos os seus livros)
+def excluir_autor(request, autor_id):
+    autor = get_object_or_404(Autor, id=autor_id)
+    autor.delete()  # Exclui o autor e, por cascade, seus livros
+    return redirect("lista_cadastros")
+
+# Excluir Livro (apaga um único livro)
+def excluir_livro(request, livro_id):
+    livro = get_object_or_404(Livro, id=livro_id)
+    livro.delete()  # Exclui apenas o livro
+    return redirect('lista_cadastros')  
+
+# Editar Livro
+def editar_livro(request, livro_id):
+    livro = get_object_or_404(Livro, id=livro_id)
+
+    if request.method == "POST":
+        form = LivroForm(request.POST, instance=livro)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_cadastros')  # Redireciona após salvar
+
+    else:
+        form = LivroForm(instance=livro)
+
+    return render(request, "editar_livro.html", {"form": form, "livro": livro})
